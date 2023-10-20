57826125f2eb8a9c6f59bb3c0250405b8075742e
SarkazW <148501629+SarkazW@users.noreply.github.com>
2023-10-20 11:53:49 +0300

    Add files via upload

---
+++ conversie.py
@@ -0,0 +1,20 @@
+print(6 // 4)
+print(6. // 4)
+
+
+print(-6 // 4)
+print(6. // -4)
+
+print(14 % 4)
+print(12 % 4.5)
+
+notaLaborator = 9
+notaExamen = 4
+notaFinala = (notaLaborator * 0.4 + notaExamen * 0.6)
+
+
+promovat = notaExamen >= 5 and notaFinala >=5
+
+
+print('Disciplina promovata:', promovat)
+print('Media: ', notaFinala)
