1a72c60bf2225a63caac44bd0337d81b19e7481b
SarkazW <148501629+SarkazW@users.noreply.github.com>
2023-11-03 08:32:31 +0000

    u

--- list.py
+++ list.py
@@ -1,3 +1,11 @@
-lst = ["D", "F", "A", "Z"]
-lst.sort()
-print(lst)
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+toFind = 5 
+found = False 
+for i in range(len(myList)): 
+    found = myList[i] == toFind 
+    if found: 
+        break 
+if found: 
+    print("Element found at index", i) 
+else: 
+    print("absent")
